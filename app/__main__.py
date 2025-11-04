945eb3c911b65af3a5c237c679be2c64173a41e8
valenthyne-ut <valenthyne.ut@gmail.com>
2025-11-04 14:56:26 +0000

    Comentar código de teste

--- app/__main__.py
+++ app/__main__.py
@@ -1,5 +1,5 @@
 from app.database import initialize_database
-from app.database.schema.UserSchema import UserSchema
+# from app.database.schema.UserSchema import UserSchema
 
 def main():
     initialize_database()
@@ -8,5 +8,5 @@ if __name__ == "__main__":
     main()
 
     # UserSchema.instance.create_one("Teste")
-    user = UserSchema.instance.find_one(1)
-    print(user)
+    # user = UserSchema.instance.find_one(1)
+    # print(user)
