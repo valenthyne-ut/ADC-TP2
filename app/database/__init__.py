9f337dd54d34b85a6145c4219ae29780c7e12894
valenthyne-ut <valenthyne.ut@gmail.com>
2025-11-11 11:44:27 +0000

    Adicionar inits que faltavam para tabelas da DB

--- app/database/__init__.py
+++ app/database/__init__.py
@@ -1,4 +1,6 @@
 import sqlite3
+from app.database.schema.ClientSchema import ClientSchema
+from app.database.schema.ContactInfoSchema import ContactInfoSchema
 from app.database.schema.UserSchema import UserSchema
 
 connection = sqlite3.connect("database.sqlite", autocommit=True)
@@ -7,4 +9,6 @@ connection = sqlite3.connect("database.sqlite", autocommit=True)
 def initialize_database():
     print("A iniciar base de dados..")
     UserSchema(connection)
+    ContactInfoSchema(connection)
+    ClientSchema(connection)
     print("Base de dados iniciada.")
