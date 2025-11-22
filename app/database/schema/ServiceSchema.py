dcdd5c04002854649450d5df64bd75416b8cc0b2
valenthyne-ut <valenthyne.ut@gmail.com>
2025-11-22 17:54:26 +0000

    Documentar schema Serviços

--- app/database/schema/ServiceSchema.py
+++ app/database/schema/ServiceSchema.py
@@ -20,7 +20,19 @@ class ServiceSchema(BaseSchema["Service"]):
             """)
         ServiceSchema.instance = self
 
-    def find_one(self, id: int | None = None, name: str | None = None):
+    def find_one(self, id: int | None = None, name: str | None = None) -> Service | None:
+        """
+        Encontra um serviço na base de dados.
+        Pelo menos um parâmetro tem de ser especificado.
+
+        :param id: O ID único do serviço.
+        :param name: O nome do serviço.
+        :type id: int
+        :type name: str
+        :return: O serviço, se foi encontrado.
+        :rtype: Service | None
+        :raises ValueError: Se nenhum parâmetro foi especificado.
+        """
         if id is None and name is None:
             raise ValueError("Um parâmetro dos 'id' ou 'name' tem de ser especificado!")
         
@@ -49,6 +61,16 @@ class ServiceSchema(BaseSchema["Service"]):
         return Service(result[0], result[1], result[2], result[3])
 
     def find_many(self, limit: int = 2000, name: str | None = None) -> list[Service]:
+        """
+        Encontra vários serviços na base de dados.
+
+        :param limit: O número máximo de resultados a devlover.
+        :param name: O nome do serviço.
+        :type limit: int
+        :type name: str
+        :return: Os serviços, se forem encontrados.
+        :rtype: list[Service]
+        """
         cursor = self._connection.cursor()
 
         base_query = """
@@ -78,6 +100,21 @@ class ServiceSchema(BaseSchema["Service"]):
         return service_list
 
     def create_one(self, name: str = "Unspecified", price: float = -1, duration_mins: int = -1) -> Service:
+        """
+        Insere um serviço na base de dados e devolve esse mesmo. Todos
+        os parâmetros são opcionais, sendo por defeito ``Unspecified``
+        ou ``-1``.
+
+        :param name: O nome do serviço a inserir.
+        :param price: O preço do serviço a inserir.
+        :param duration_mins: A duração do serviço (em minutos) a inserir.
+        :type name: str
+        :type price: float
+        :type duration_mins: int
+        :return: O serviço inserido.
+        :rtype: Service
+        :raises ValueError: Se algo correu mal ao inserir o serviço na base de dados.
+        """
         cursor = self._connection.cursor()
         cursor.execute("""
             INSERT INTO Service(name, price, duration_mins)
@@ -94,6 +131,14 @@ class ServiceSchema(BaseSchema["Service"]):
         return Service(cursor.lastrowid, name, price, duration_mins)
 
     def delete(self, instance: Service) -> bool:
+        """
+        Apaga um serviço da base de dados.
+
+        :param instance: Uma instância de um serviço.
+        :type instance: Service
+        :return: Um booleano que representa se o serviço foi apagado ou não.
+        :rtype: bool
+        """
         cursor = self._connection.cursor()
         cursor.execute("""
             DELETE FROM Service
