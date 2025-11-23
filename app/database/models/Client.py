27889a829f952cfc52582cb455db57fa63034185
valenthyne-ut <valenthyne.ut@gmail.com>
2025-11-23 14:28:05 +0000

    Documentar model Cliente

--- app/database/models/Client.py
+++ app/database/models/Client.py
@@ -4,6 +4,9 @@ from app.database.models.Base import Base
 @dataclass
 class Client(Base["Client"]):
     contact_id: int
+    """
+    O ID da informação de contactos (:class:`app.database.models.ContactInfo`) do cliente.
+    """
 
     def __str__(self) -> str:
         return f"ID de utilizador: {self.id}\nID de informação de contacto: {self.contact_id}"
--- docs/app.database.models.Client.rst
+++ docs/app.database.models.Client.rst
@@ -1,5 +1,7 @@
-app.database.models.Client module
+Cliente
 =================================
+
+Representa um conjunto de dados de um cliente.
 
 .. automodule:: app.database.models.Client
    :members:
