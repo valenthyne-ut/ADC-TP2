df365c57fc984c1e5f415ff2163a784e161e4889
valenthyne-ut <valenthyne.ut@gmail.com>
2025-11-23 13:20:05 +0000

    Importar utilizadores para a base de dados

--- app/__main__.py
+++ app/__main__.py
@@ -1,9 +1,9 @@
-from datetime import date, time
+# from datetime import date, time
 from app.database import initialize_database
-from app.database.schema.AppointmentSchema import AppointmentSchema
+# from app.database.schema.AppointmentSchema import AppointmentSchema
 from app.database.schema.ClientSchema import ClientSchema
 from app.database.schema.ContactInfoSchema import ContactInfoSchema
-from app.database.schema.ServiceSchema import ServiceSchema
+# from app.database.schema.ServiceSchema import ServiceSchema
 from app.database.schema.TechnicianSchema import TechnicianSchema
 from app.database.schema.UserSchema import UserSchema
 
@@ -14,16 +14,40 @@ def main():
 
     dados = carregar_dados()
     for user in dados.get("Utilizadores"):
-        UserSchema.instance.create_one(
-            alias=user["Email"],
-            full_name=user["Nome"]
-        )
-        
-    for user in dados.get("Técnicos"):
-        UserSchema.instance.create_one(
-            alias=user["Email"],
-            full_name=user["Nome"]
-        )
+        if user is not None:
+            db_user = UserSchema.instance.create_one(
+                alias=user["Email"],
+                full_name=user["Nome"]
+            )
+
+            db_contact_info = ContactInfoSchema.instance.create_one(
+                email=db_user.alias,
+                address=user["Morada"],
+                phone_num=user["Contacto"]
+            )
+
+            ClientSchema.instance.create_one(
+                db_user.id,
+                db_contact_info.id
+            )
+
+    for technician in dados.get("Técnicos"):
+        if technician is not None:
+            db_user = UserSchema.instance.create_one(
+                alias=technician["Email"],
+                full_name=technician["Nome"]
+            )
+
+            db_contact_info = ContactInfoSchema.instance.create_one(
+                email=db_user.alias,
+                address=technician["Morada"],
+                phone_num=technician["Contacto"]
+            )
+
+            TechnicianSchema.instance.create_one(
+                db_user.id,
+                db_contact_info.id
+            )
     
 
 if __name__ == "__main__":
